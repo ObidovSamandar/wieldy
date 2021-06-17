59f6c9f126855be91a298e28f64a0c20358b4f1e
Samandar Obidov <samandarobidovfrd@gmail.com>
2021-06-17 23:49:10 +0500

    updated; ;

--- src/server.js
+++ src/server.js
@@ -30,4 +30,4 @@ glob('**/*Route.js', {realpath:true},(err, files)=>{
 })
 
 
-app.listen(configs.HTTP_PORT || 8045, console.log(`SERVER RUNNING ON PORT ${configs.HTTP_PORT}`))
+app.listen(configs.HTTP_PORT || 8045,"0.0.0.0", console.log(`SERVER RUNNING ON PORT ${configs.HTTP_PORT}`))
