14a074caf2118c6f0b1e3816529c9b566c57885d
Samandar Obidov <samandarobidovfrd@gmail.com>
2021-06-18 04:45:43 +0500

    created

--- src/store/mongo.js
+++ src/store/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const configs = require('../config/configs')
 
 module.exports = async function (){
-    return await mongoose.connect(configs.MongoDB_URI, {
+    return await mongoose.connect(configs.MongoDB_URI || "mongodb://localhost/wieldy", {
         useNewUrlParser:true,
         useUnifiedTopology:true,
         useFindAndModify:false,
